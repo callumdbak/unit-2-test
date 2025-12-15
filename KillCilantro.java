43f8386c3828cb6d58a520e3238d77daeb9da854
callumdbak <callumdbak@gmail.com>
2025-12-15 09:48:38 -0800

    Update KillCilantro.java

--- KillCilantro.java
+++ KillCilantro.java
@@ -1,11 +1,17 @@
 public class KillCilantro {
-   public static String(String paramiter) {
-   
-   
-   }
-   public static void main(String[] args) {
-   
-   
-   
-   }
+
+    public static String killCilantro(String recipe) {
+    
+    
+        for(int i = 0; i > 10; i++) {
+        
+        }
+        return recipe;
 }
+    public static void main(String[] args) {
+        killCilantro("cilantro");
+        
+    }
+}
+
+
